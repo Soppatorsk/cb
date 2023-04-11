5b704b6929c0a33159121087fbbaf3aecefdc678
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-04-11 16:23:09 +0200

    project files

---
+++ code/Coolbooks/Pages/CoolBookContext.cs
@@ -0,0 +1,9 @@
+﻿using Coolbooks.Models;
+
+namespace Coolbooks.Pages
+{
+    internal class CoolBookContext
+    {
+        public IEnumerable<Book> Books { get; internal set; }
+    }
+}
---
+++ code/Coolbooks/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Coolbooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CoolbooksContext _db;
+        public IEnumerable<Book> Books { get; set; }
+
+        public IndexModel(CoolbooksContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Books = _db.Books;
+        }
+    }
+}
