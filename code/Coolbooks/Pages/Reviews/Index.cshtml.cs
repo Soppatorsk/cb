8a9576c0e271e986ec4b51fbdec2b62abafcf111
Emanuelskod <Backhax@hotmail.com>
2023-04-12 09:26:00 +0200

    Added folders with index pages

---
+++ code/Coolbooks/Pages/Reviews/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages.Reviews
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
