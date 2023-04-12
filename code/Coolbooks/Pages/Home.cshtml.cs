6edf0379e818f374cc3e2c0c0a8f9552d785878d
Julia <derpette87@gmail.com>
2023-04-12 15:53:52 +0200

    Home page + navbar items
    Starting with displaying a page of newest books and a "hero book".

---
+++ code/Coolbooks/Pages/Home.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Coolbooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages
+{
+    public class HomeModel : PageModel
+    {
+        private readonly CoolbooksContext _db;
+        public IEnumerable<Book> Books { get; set; }
+
+        public HomeModel(CoolbooksContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Books = _db.Books;
+        }
+    }
+}
