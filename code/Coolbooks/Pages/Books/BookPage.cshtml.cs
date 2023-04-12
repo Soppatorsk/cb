54b26498f8d73b8ab121df6c5f98e1857ce2e457
Emanuelskod <Backhax@hotmail.com>
2023-04-12 12:24:39 +0200

    BookPage updates

--- code/Coolbooks/Pages/Books/BookPage.cshtml.cs
+++ code/Coolbooks/Pages/Books/BookPage.cshtml.cs
@@ -1,6 +1,7 @@
 using Coolbooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coolbooks.Pages.Books
 {
@@ -8,6 +9,7 @@ namespace Coolbooks.Pages.Books
     {
         private readonly CoolbooksContext _db;
         public Book Book { get; set; }
+        //public IEnumerable<Book> Book { get; set; }
 
         public BookPageModel(CoolbooksContext db)
         {
@@ -15,7 +17,13 @@ namespace Coolbooks.Pages.Books
         }
         public void OnGet(int id)
         {
+            //Book = _db.Books.Include("Genre").Find(id);
             Book = _db.Books.Find(id);
+            //Book = _db.Books.Include(b => b.Genre).FirstOrDefault(b => b.GenreId == id);
+            //Book = _db.Books
+            //    .Include(b => b.Genre)
+            //    .FirstOrDefault(b => b.GenreId == id)
+            ;
         }
     }
 }
