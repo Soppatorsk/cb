ee03b512d01ad712b77bcdd50e7f06f754f7334d
Emanuelskod <Backhax@hotmail.com>
2023-04-12 11:01:26 +0200

    Books update

--- code/Coolbooks/Pages/Books/Index.cshtml.cs
+++ code/Coolbooks/Pages/Books/Index.cshtml.cs
@@ -1,12 +1,26 @@
+using Coolbooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coolbooks.Pages.Books
 {
     public class IndexModel : PageModel
     {
+        private readonly CoolbooksContext _db;
+        public IEnumerable<Book> Books { get; set; }
+
+        public IndexModel(CoolbooksContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Books = _db.Books
+                .Include("Genre")
+                .Include("Author")
+                .ToList();
         }
     }
 }
--- code/Coolbooks/Pages/Index.cshtml.cs
+++ code/Coolbooks/Pages/Index.cshtml.cs
@@ -7,16 +7,14 @@ namespace Coolbooks.Pages
     public class IndexModel : PageModel
     {
         private readonly CoolbooksContext _db;
-        public IEnumerable<Book> Books { get; set; }
 
         public IndexModel(CoolbooksContext db)
         {
-            _db = db;
+           
         }
 
         public void OnGet()
         {
-            Books = _db.Books;
         }
     }
 }
