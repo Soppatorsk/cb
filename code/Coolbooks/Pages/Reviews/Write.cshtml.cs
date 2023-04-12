c851c89a75ffd7311bba3bb139a9fe4be74d3ebe
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-04-12 12:11:51 +0200

    Review template

---
+++ code/Coolbooks/Pages/Reviews/Write.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages.Reviews
+{
+    public class WriteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
