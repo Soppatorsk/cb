c73e7d8b0dc46a87352a08a2101373db0348dd37
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-04-13 12:43:52 +0200

    reviews overview basic

--- code/Coolbooks/Pages/Reviews/Index.cshtml.cs
+++ code/Coolbooks/Pages/Reviews/Index.cshtml.cs
@@ -1,14 +1,23 @@
 using Coolbooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coolbooks.Pages.Reviews
 {
 	public class IndexModel : PageModel
 	{
+		private readonly CoolbooksContext _db;
+		public Book Book { get; set; }
+		public IEnumerable<Review> Reviews { get; set; } 
+
+		public IndexModel(CoolbooksContext db) => _db = db;
+
 		public void OnGet()
 		{
-
+			Reviews = _db.Reviews
+				.Include("Book")
+				.ToList();
 		}
 	}
 }
