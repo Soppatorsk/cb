2668f2eaefe66e56098b6696a19d26d749b635b8
Soppatorsk <simon.kevlarsjal@gmail.com>
2023-04-17 20:20:13 +0200

    see likes and dislikes on review, like/dislike button

--- code/Coolbooks/Pages/Reviews/Read.cshtml.cs
+++ code/Coolbooks/Pages/Reviews/Read.cshtml.cs
@@ -11,8 +11,14 @@ namespace Coolbooks.Pages.Reviews
         public Book Book { get; set; }
         public Review Review { get; set; }
         public SiteUser SiteUser { get; set; }
+        public Like NewLike = new Like();
+
+
         public string UserFullName { get; set; }
         public string AuthorFullName = string.Empty;
+        public int Likes = 0;
+        public int Dislikes = 0;
+
         private readonly CoolbooksContext _db;
         public ReadModel(CoolbooksContext db) => _db = db;
 
@@ -27,13 +33,19 @@ namespace Coolbooks.Pages.Reviews
                 .Include("Author")
                 .FirstOrDefault(b => b.BookId == Review.BookId);
 
-
             SiteUser = _db.SiteUsers
                 .Include("Userinfo")
                 .FirstOrDefault(b => b.UserId == Review.UserId);
 
             UserFullName = SiteUser.Userinfo.Firstname + " " + SiteUser.Userinfo.Lastname;
             AuthorFullName = Review.Book.Author.Firstname + " " + Review.Book.Author.Lastname;
+
+            Likes = _db.Likes.Where(x => x.ReviewId == id && x.Like1 == "Like")
+                .Count();
+
+            Dislikes = _db.Likes.Where(x => x.ReviewId == id && x.Like1 == "Dislike")
+                .Count();
+
 
         }
         public void OnGet(int id)
@@ -61,6 +73,22 @@ namespace Coolbooks.Pages.Reviews
             Review.Status = "Removed";
             _db.SaveChanges();
 
+        }
+
+        public void OnPostLike(int id)
+        {
+            LoadPage(id);
+
+            Review = _db.Reviews.Where(r => r.ReviewId == id).FirstOrDefault();  
+            
+                NewLike.ReviewId = id;
+                NewLike.Like1 = Request.Form["Vote"];
+
+                //TODO user
+                NewLike.UserId = 1;
+
+                if (true /* TODO User no previous vote */ ) _db.Add(NewLike);
+                _db.SaveChanges();
         }
 
     }
