9b9a2a1c11a0c505a0d12c9362404aff651b9af7
Julia <derpette87@gmail.com>
2023-04-18 14:48:45 +0200

    Admin submenu with empty pages

---
+++ code/Coolbooks/Pages/Admin/AddAuthor.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Coolbooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages
+{
+    public class AddAuthorModel : PageModel
+    {
+        private readonly CoolbooksContext _db;
+
+        public AddAuthorModel(CoolbooksContext db)
+        {
+           
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ code/Coolbooks/Pages/Admin/AddGenre.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Coolbooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages
+{
+    public class AddGenreModel : PageModel
+    {
+        private readonly CoolbooksContext _db;
+
+        public AddGenreModel(CoolbooksContext db)
+        {
+           
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
