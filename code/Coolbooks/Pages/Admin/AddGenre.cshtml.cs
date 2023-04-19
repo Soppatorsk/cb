b6ebe99228ef364780c24e43a9e26107849abe95
Julia <derpette87@gmail.com>
2023-04-19 18:09:58 +0200

    Setting up admin controls for adding new genre

--- code/Coolbooks/Pages/Admin/AddGenre.cshtml.cs
+++ code/Coolbooks/Pages/Admin/AddGenre.cshtml.cs
@@ -1,20 +1,44 @@
 ﻿using Coolbooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coolbooks.Pages
 {
     public class AddGenreModel : PageModel
     {
         private readonly CoolbooksContext _db;
+        public IEnumerable<Genre> Genres { get; set; }
 
         public AddGenreModel(CoolbooksContext db)
         {
-           
+           _db = db;
+        }
+        [BindProperty]
+        public Genre Genre { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Genres.Add(Genre);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("/Admin/Index");
+        }
+
+        public class ListGenres
+        {
+            public string SelectGenres { get; set; }
         }
 
         public void OnGet()
         {
+            Genres = _db.Genres.ToList();
+
         }
     }
 }
---
+++ code/Coolbooks/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Coolbooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages
+{
+    public class Index : PageModel
+    {
+        private readonly CoolbooksContext _db;
+
+        public Index(CoolbooksContext db)
+        {
+           
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
