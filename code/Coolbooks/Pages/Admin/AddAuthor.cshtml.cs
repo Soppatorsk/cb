b256131060d4459dea6f0bf1ce1c51c725b87cae
Julia <derpette87@gmail.com>
2023-04-24 10:27:15 +0200

    New pages for add

--- code/Coolbooks/Pages/Admin/AddAuthor.cshtml.cs
+++ code/Coolbooks/Pages/Admin/AddAuthor.cshtml.cs
@@ -7,14 +7,37 @@ namespace Coolbooks.Pages
     public class AddAuthorModel : PageModel
     {
         private readonly CoolbooksContext _db;
+        public IEnumerable<Author> Authors { get; set; }
 
         public AddAuthorModel(CoolbooksContext db)
         {
-           
+            _db = db;
+        }
+        [BindProperty]
+        public Author Author { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Authors.Add(Author);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("/Admin/Index");
+        }
+
+        public class ListAuthors
+        {
+            public string SelectAuthors { get; set; }
         }
 
         public void OnGet()
         {
+            Authors = _db.Authors.ToList();
+
         }
     }
 }
--- code/Coolbooks/Pages/Admin/AddBook.cshtml.cs
+++ code/Coolbooks/Pages/Admin/AddBook.cshtml.cs
@@ -1,20 +1,47 @@
 ﻿using Coolbooks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Coolbooks.Pages
 {
     public class AddBookModel : PageModel
     {
         private readonly CoolbooksContext _db;
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Author> Authors { get; set; } //nytt
 
         public AddBookModel(CoolbooksContext db)
         {
-           
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Books.Add(Book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("/Admin/Index");
+        }
+
+        public class ListBooks
+        {
+            public string SelectBooks { get; set; }
+
         }
 
         public void OnGet()
         {
+            Books = _db.Books.Include("Author").Include("Genre").ToList();
+            Authors = _db.Authors.ToList(); //nytt
+
         }
     }
 }
