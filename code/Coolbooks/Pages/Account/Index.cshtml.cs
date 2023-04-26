4356af172aa58fe50f65944d9be65b6d08c1042c
Kimpan261 <124882104+Kimpan261@users.noreply.github.com>
2023-04-26 11:34:47 +0200

    Books fix

--- code/Coolbooks/Pages/Account/Index.cshtml.cs
+++ code/Coolbooks/Pages/Account/Index.cshtml.cs
@@ -22,7 +22,7 @@ namespace Coolbooks.Pages.Account
 
         public async Task OnGetAsync()
         {
-            if (!User.Identity.IsAuthenticated)
+            if (User.Identity.IsAuthenticated)
             {
                 Userinfo = await _context.Userinfos.ToListAsync();
             }
