1018a18379f65d5d5089fde787189cf97287f5cb
Kimpan261 <124882104+Kimpan261@users.noreply.github.com>
2023-05-04 23:12:05 +0200

    Roles

--- code/Coolbooks/Pages/RolesManager/Assign.cshtml.cs
+++ code/Coolbooks/Pages/RolesManager/Assign.cshtml.cs
@@ -1,12 +1,65 @@
+using Coolbooks.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
 
 namespace Coolbooks.Pages.RolesManager
 {
-    public class AssignModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
-}
+	//[Authorize(Roles ="Admin")]
+	//[Authorize(Roles = "User,Admin,Moderator")]
+	public class AssignModel : PageModel
+	{
+		private readonly RoleManager<IdentityRole> _roleManager; 
+		private readonly UserManager<IdentityUser> _userManager; 
+		public AssignModel(RoleManager<IdentityRole> 
+		roleManager, UserManager<IdentityUser> userManager) 
+		{ 
+		_roleManager = roleManager; 
+		_userManager = userManager; 
+		} 
+	public SelectList Roles { get; set; }
+	public SelectList Users { get; set; }
+	[BindProperty, Required, Display(Name = "Role")]
+	public string SelectedRole { get; set; }
+	[BindProperty, Required, Display(Name = "User")]
+	public string SelectedUser { get; set; }
+		[Authorize(Roles = "Admin")]
+		public async Task OnGet()
+	{
+		await GetOptions();
+	}
+		[Authorize(Roles = "Admin")]
+		public async Task<IActionResult> OnPostAsync()
+	{
+		if (ModelState.IsValid)
+		{
+			var user = await _userManager.FindByNameAsync(SelectedUser);
+	await _userManager.AddToRoleAsync(user, SelectedRole); 
+	return RedirectToPage("/RolesManager/Index");
+		}
+		await GetOptions();
+		return Page();
+	}
+	public async Task GetOptions() 
+	{
+	var roles = await _roleManager.Roles.ToListAsync(); 
+	var users = await _userManager.Users.ToListAsync(); 
+	Roles = new SelectList(roles, nameof(IdentityRole.Name)); 
+	Users = new SelectList(users, nameof(IdentityUser.UserName));
+	}
+	}
+	}
+
+
+
+
+
+
+
+	
