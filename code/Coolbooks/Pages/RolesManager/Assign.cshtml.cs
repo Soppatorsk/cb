98a030220b8bbc307388322213b2c2eadc290b39
Kimpan261 <124882104+Kimpan261@users.noreply.github.com>
2023-05-04 14:26:19 +0200

    Roles pack1

---
+++ code/Coolbooks/Pages/RolesManager/Assign.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages.RolesManager
+{
+    public class AssignModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ code/Coolbooks/Pages/RolesManager/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coolbooks.Pages.RolesManager
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public IndexModel(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public List<IdentityRole> Roles { get; set; }
+        public void OnGet()
+        {
+            Roles = _roleManager.Roles.ToList();
+        }
+    }
+}
