2b9d11b462125f5ea929c7f65cd2efc1655358f7
Julia <derpette87@gmail.com>
2023-05-05 15:05:01 +0200

    Making "Home" the default page

--- code/Coolbooks/Program.cs
+++ code/Coolbooks/Program.cs
@@ -5,6 +5,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//this maked the "Home" page to be the default landing page, instead of "Index".
+//have excluded "Index" from project
+builder.Services.AddMvc().AddRazorPagesOptions(options =>
+{
+    options.Conventions.AddPageRoute("/Home", "");
+});
+
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -37,6 +45,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
